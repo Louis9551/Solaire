a7812c68b77b9ece4dcbb2aadfd7f276f348de73
Ciolultra13 <131893654+Ciolultra13@users.noreply.github.com>
2024-01-02 13:10:34 +0100

    Code à rendre

--- TOURSOLAIRE.py
+++ TOURSOLAIRE.py
@@ -1,7 +1,7 @@
 # Données
-Pel = 538 # kW Puissance électrique nominale
+Pel = 538.56# kW Puissance électrique nominale
 Ta = 25+273 # °K Température ambiante
-Tf = 450+273 # °K Température de fonctionnement
+Tf = 250+273 # °K Température de fonctionnement
 Alpha_sc = 0.7 # Absorptivité du capteur
 Epsilon_sc = 0.85 # Emissivité du capteur
 S = 1000 # W/m² Irradiation solaire
@@ -19,9 +19,10 @@ print("etaSC = ",eta_sc)
 Qsc = Pel/eta_sc/eta_c
 print("Qsc = ",Qsc,"kW")
 Stot = Qsc*1000/S
-print("Stot = ",Stot,"m²")
 NbElio = round(Stot/Selio)
 print("NbElio = ",NbElio)
+Stot = NbElio*Selio
+print("Stot = ",Stot,"m²")
 Prix = PrixSeville*NbElio/NbElioSeville
 print("Prix = ",Prix,"€")
 
